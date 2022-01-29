9e87683697bf5c41e643cf90e9a32eed86b20d4a
nakajima1999 <s1711547@klis.tsukuba.ac.jp>
2022-01-29 21:38:36 +0900

    context_processors.py

--- myproject/myproject/context_processors.py
+++ myproject/myproject/context_processors.py
@@ -1,6 +1,6 @@
 from django.db.models import Count, Q
 
-from blog.models import Cat, Feature
+from blog.models import Cat, Feature, Place
 
 
 def common(request):
@@ -8,6 +8,8 @@ def common(request):
         'cats': Cat.objects.annotate(
             num_posts=Count('post', filter=Q(post__is_public=True))),
         'features': Feature.objects.annotate(
+            num_posts=Count('post', filter=Q(post__is_public=True))),
+        'places': Place.objects.annotate(
             num_posts=Count('post', filter=Q(post__is_public=True))),
     }
     return context
