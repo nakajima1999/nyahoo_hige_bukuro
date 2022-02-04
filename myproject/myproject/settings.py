2c609185973ec07bbebdf79d114b7a100a510f22
nakajima1999 <s1711547@klis.tsukuba.ac.jp>
2022-02-04 22:15:37 +0900

    settings

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -67,6 +67,11 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'myproject.context_processors.common',
             ],
+
+        'libraries': {
+                'myfilters': 'templatetags.myfilters' # 追記
+            }
+
         },
     },
 ]
