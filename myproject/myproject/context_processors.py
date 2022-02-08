bfa8013eed7394578c18a82318df2ddcbe3363ff
nakajima1999 <s1711547@klis.tsukuba.ac.jp>
2022-02-08 22:35:35 +0900

    myproject/context_processors.py

--- myproject/myproject/context_processors.py
+++ myproject/myproject/context_processors.py
@@ -1,6 +1,8 @@
 from django.db.models import Count, Q
+from django.utils import timezone
+from django.db.models.functions import TruncMonth
 
-from blog.models import Cat, Feature, Place
+from blog.models import Cat, Feature, Place, Post
 
 
 def common(request):
@@ -11,5 +13,9 @@ def common(request):
             num_posts=Count('post', filter=Q(post__is_public=True))),
         'places': Place.objects.annotate(
             num_posts=Count('post', filter=Q(post__is_public=True))),
+        #'dates': Post.objects.annotate(month=TruncMonth('discovery_date')).values('month').annotate(count=Count('pk'))
+
+        'dates': Post.objects.dates('discovery_date', 'month', order='DESC'),
+    
     }
     return context
